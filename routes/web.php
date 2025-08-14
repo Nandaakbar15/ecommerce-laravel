e94e20eb33f2938799efb5725bfb37945accfe76
Alvinda_akbar10 <89963240+Nandaakbar15@users.noreply.github.com>
2025-08-14 20:38:47 +0700

    Menginstall breeze untuk halaman login dan register

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,20 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view("home");
-// });
-
-Route::get('/', function() {
-    return redirect('/admin/home');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::prefix("admin")->group(function() {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::get('/home', [AdminController::class, 'home']);
-
-    // route product admin
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/addnewproducts', [ProductController::class, 'create']);
-
-    // route karyawan admin
-    Route::get('/employee', [EmployeeController::class, 'index']);
-    Route::get('/addnewkaryawan', [EmployeeController::class, 'create']);
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::prefix("customer")->group(function() {
-    Route::get("/home", [CustomerController::class, 'home']);
-
-    Route::get('/listproducts', [CustomerController::class, 'listproducts']);
-
-    Route::get('/carts', [CustomerController::class, 'listcarts']);
-});
+require __DIR__.'/auth.php';
